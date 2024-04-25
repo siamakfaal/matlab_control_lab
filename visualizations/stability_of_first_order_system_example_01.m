7be9917f5fbb3fbbebf4e4dd722efd12781f7823
Siamak Faal <siamak.faal@gmail.com>
2024-04-25 11:06:36 -0400

    corrects first order example 1

--- visualizations/stability_of_first_order_system_example_01.m
+++ visualizations/stability_of_first_order_system_example_01.m
@@ -1,23 +1,42 @@
 clc; clear; close all;
 
-dx = @(t, x) (4*x - x^3)*exp(-0.3*x^2);
+f = @(t, x) (4*x - x.^3).*exp(-0.3*x.^2);
 
 tspan = linspace(0,3)';
-
-x0 = -3;
-
-
-ax = axes(NextPlot='add', Box='on',...
-    Xlim=tspan([1,end]), Ylim=[-3, 3],...
-    TickLabelInterpreter='latex', FontSize=20);
-
-xlabel(ax, '$t$', Interpreter='latex', FontSize=20);
-ylabel(ax, '$x(t)$', Interpreter='latex', FontSize=20);
+x0 = -3:0.02:3;
+dx = f([], x0);
 
 colors = parula;
+ax = buildaxes(x0([1,end]), 1.2*[min(dx), max(dx)], tspan([1,end]));
 
-while x0 <= 3
-    [~,x] = ode45(dx, tspan, x0);
-    plot(ax, tspan, x, Color=colors(randi(length(colors)),:), LineWidth=1);
-    x0 = x0 + 0.02;
+plot(ax(1),dx,x0,LineWidth=2);
+I = 1:20:length(x0);
+quiver(ax(1), zeros(size(I)), x0(I), zeros(size(I)), sign(dx(I)), 0.2,...
+    LineWidth=2);
+yline(ax(1),[0 -2 2]);
+
+for k=1:length(x0)
+    [~,x] = ode45(f, tspan, x0(k));
+    plot(ax(2), tspan, x, ...
+        Color=colors(randi(length(colors)),:), ...
+        LineWidth=2);
+end
+
+
+function ax = buildaxes(xrange, dxrange, trange)
+ax(1) = subplot(1, 2, 1, NextPlot='add', Box='on',...
+    Xlim=xrange, Ylim=dxrange,...
+    TickLabelInterpreter='latex', FontSize=20);
+ax(2) = subplot(1, 2, 2, NextPlot='add', Box='on',...
+    Xlim=trange, Ylim=xrange,...
+    TickLabelInterpreter='latex', FontSize=20);
+
+linkaxes([ax(1), ax(2)], 'y');
+
+xlabel(ax(1), '$\dot{x}(t)$', Interpreter='latex', FontSize=20);
+ylabel(ax(1), '$x(t)$', Interpreter='latex', FontSize=20);
+xlabel(ax(2), '$t$', Interpreter='latex', FontSize=20);
+ylabel(ax(2), '$x(t)$', Interpreter='latex', FontSize=20);
+
+xline(ax(1),0);
 end
